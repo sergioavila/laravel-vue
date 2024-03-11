a054bcf4ea87a71fe52948aeb8f88e514fae6d17
Checho <sergio.avila.astorga@gmail.com>
2024-03-11 19:24:22 -0300

    add fillable fields in model

--- app/Models/DataUf.php
+++ app/Models/DataUf.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class DataUf extends Model
 {
     use HasFactory;
-    
+    protected $table = 'data_ufs'; // protegido para que no se pueda modificar
+    protected $fillable = ['date', 'value']; // campos que se pueden modificar desde el front
+
 }
