2dd29ca9e5b67562f494478bff401931b505dd2c
Checho <sergio.avila.astorga@gmail.com>
2024-03-11 19:23:36 -0300

    fix database

--- database/migrations/2024_03_11_171228_create_data_ufs_table.php
+++ database/migrations/2024_03_11_171228_create_data_ufs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('data_ufs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->date('date');
+            $table->date('date')->unique();
             $table->string('value');
         });
     }
