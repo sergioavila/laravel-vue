452d377ba94779fb607d9c7b8ac9eba3b8f1acd8
Checho <sergio.avila.astorga@gmail.com>
2024-03-11 15:16:40 -0300

    add charts and fix vite base

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,4 +1,33 @@
 <template>
-    <h1>Indicadores</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum laudantium, repellat asperiores omnis nemo vero maiores! Ipsam ut tempore dignissimos perspiciatis, dolores rem magnam maiores quas facilis necessitatibus eum dolorem.</p>
+    <div class="container">
+        <h1>Indicadores</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum laudantium, repellat asperiores omnis nemo vero maiores! Ipsam ut tempore dignissimos perspiciatis, dolores rem magnam maiores quas facilis necessitatibus eum dolorem.</p>
+        <Bar
+            id="indicadores"
+            :options="chartOptions"
+            :data="chartData"
+        />
+    </div>
 </template>
+
+<script>
+    import { Bar } from 'vue-chartjs'
+    import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+    ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+    export default {
+        name: 'BarChart',
+        components: { Bar },
+        data() {
+            return {
+                chartData: {
+                    labels: [ 'January', 'February', 'March' ],
+                    datasets: [ { data: [40, 20, 12] } ]
+                },
+                chartOptions: {
+                    responsive: true
+                }
+            }
+        }
+    }
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import "./bootstrap";
+// import Vue from "vue";
 import { createApp } from "vue";
+// import store for use in the app;
 import store from "./store/index.js";
+// import the app main component
 import App from "./App.vue";
 
+//Create the app and mount it to the #app element
 createApp(App).mount("#app");
